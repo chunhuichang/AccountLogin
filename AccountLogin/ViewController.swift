89216f7bfc8c9c0f7e58f2e849ac0dcc6e39c64a
Chun Hui Chang <jill20021@gmail.com>
2022-06-18 17:40:05 +0800

    feat: change rootViewController

--- AccountLogin/ViewController.swift
+++ AccountLogin/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let viewModel: String
+    
+    init(viewModel: String) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(displayP3Red: 224.0/255.0, green: 216.0/255.0, blue: 200.0/255.0, alpha: 1)
     }
 
 
