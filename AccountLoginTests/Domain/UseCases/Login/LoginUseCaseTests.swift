4dcf5e11bde49ff56b1c0df501f93a09bda763cd
Chun Hui Chang <jill20021@gmail.com>
2022-06-19 15:42:40 +0800

    test: Create login use case

---
+++ AccountLoginTests/Domain/UseCases/Login/LoginUseCaseTests.swift
@@ -0,0 +1,13 @@
+//
+//  LoginUseCaseTests.swift
+//  AccountLoginTests
+//
+//  Created by Jill Chang on 2022/6/19.
+//
+
+import XCTest
+
+class LoginUseCaseTests: XCTestCase {
+    func testExample() {
+    }
+}
